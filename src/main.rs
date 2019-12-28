5906dd7f679942282bf24907db95fbabda4e0052
foobles <buzdav2@gmail.com>
2019-12-27 22:41:29 -0600

    added summing of module fuel requirements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, BufReader, prelude::*};
+
+fn module_fuel_req(mass: i32) -> i32 {
+    mass / 3 - 2
+}
+
+fn main() -> io::Result<()> {
+    let file = File::open("data/module_mass.txt")?;
+    let reader = BufReader::new(file);
+    let total_fuel_req: io::Result<i32> = reader
+        .lines()
+        .try_fold(0, |acc, line| {
+            Ok(acc + module_fuel_req(line?.parse::<i32>().unwrap()))
+        });
+
+    println!("Total fuel requirement: {}", total_fuel_req?);
+    Ok(())
 }
