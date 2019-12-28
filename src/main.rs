dc89058d39c62d78d21b6b8e6e67914be7c05b9d
foobles <buzdav2@gmail.com>
2019-12-28 16:15:17 -0600

    found noun+verb giving output 19690720

--- src/main.rs
+++ src/main.rs
@@ -26,15 +26,24 @@ fn main() -> io::Result<()> {
 
     let intcode_file = File::open("data/intcode_prog.txt")?;
     let intcode_reader = BufReader::new(intcode_file);
-    let code: Vec<_> = intcode_reader
+    let mut code: Vec<_> = intcode_reader
         .split(b',')
         .map(|x| {
                 String::from_utf8(x.unwrap()).unwrap().parse::<i32>().unwrap()
         })
         .collect();
 
-    let mut machine = intcode::Machine::new(code);
-    println!("code result: {}", machine.run().unwrap());
-
+    for noun in 0..code.len() {
+        for verb in 0..code.len() {
+            code[1] = noun as i32;
+            code[2] = verb as i32;
+            let mut machine = intcode::Machine::new(code.clone());
+            if let Ok(n) = machine.run() {
+                if n == 19690720 {
+                    println!("Noun={}, Verb={}", noun, verb);
+                }
+            }
+        }
+    }
     Ok(())
 }
