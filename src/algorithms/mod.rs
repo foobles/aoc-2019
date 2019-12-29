449173fa0f11d44827667fb4ca298be5baed5485
foobles <buzdav2@gmail.com>
2019-12-28 21:49:28 -0600

    added AaVec

--- src/algorithms/mod.rs
+++ src/algorithms/mod.rs
@@ -1,10 +1,49 @@
 mod tests;
 
 use std::iter;
+use std::str::FromStr;
 
-fn module_fuel_req(mass: i32) -> i32 {
+pub fn module_fuel_req(mass: i32) -> i32 {
     iter::successors(Some(mass / 3 - 2), |&cur| {
         Some(cur / 3 - 2).filter(|&x| x > 0)
     })
     .sum()
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+enum Dir {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct AaVec {
+    dir: Dir,
+    magnitude: i32
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct ParseAaVecError;
+
+impl FromStr for AaVec {
+    type Err = ParseAaVecError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let first = s.chars().next().ok_or(ParseAaVecError)?;
+        let dir = match  first {
+            'U' => Dir::Up,
+            'D' => Dir::Down,
+            'L' => Dir::Left,
+            'R' => Dir::Right,
+            _ => return Err(ParseAaVecError)
+        };
+        let magnitude = s[first.len_utf8()..].parse().or(Err(ParseAaVecError))?;
+        Ok(AaVec{ dir, magnitude })
+    }
+}
+
+pub fn closest_wire_intersection(a: &[AaVec], b: &[AaVec]) -> i32 {
+  unimplemented!()
 }
--- src/algorithms/tests.rs
+++ src/algorithms/tests.rs
@@ -8,3 +8,21 @@ fn algo_module_fuel_req() {
     assert_eq!(module_fuel_req(1969), 966);
     assert_eq!(module_fuel_req(100756), 50346);
 }
+
+
+#[test]
+fn algo_parse_aa_vec() {
+    assert_eq!("U432".parse::<AaVec>().unwrap(), AaVec{ dir: Dir::Up, magnitude: 432});
+}
+
+#[test]
+#[should_panic]
+fn algo_parse_aa_vec_no_dir() {
+    "123".parse::<AaVec>().unwrap();
+}
+
+#[test]
+#[should_panic]
+fn algo_parse_aa_vec_no_magnitude() {
+    "D".parse::<AaVec>().unwrap();
+}
