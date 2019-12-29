4d29771ac3a4802239b27dc8c69be3e0275cb450
foobles <buzdav2@gmail.com>
2019-12-28 20:04:30 -0600

    fixed test name

--- src/intcode/tests.rs
+++ src/intcode/tests.rs
@@ -33,7 +33,7 @@ fn intcode_machine_modify_after_end() {
 }
 
 #[test]
-fn intcode_machine_modify_before_and_after() {
+fn intcode_machine_modify_before() {
     test_machine_states(
         &[1,1,1,4,99,5,6,0,99],
         &[30,1,1,4,2,5,6,0,99]
