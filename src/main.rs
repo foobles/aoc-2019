28cdc77087faf88c7fd51448b82df3ef7062fbe9
foobles <buzdav2@gmail.com>
2019-12-29 15:10:36 -0600

    solved closest wire intersection

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,16 @@ mod algorithms;
 use std::fs::File;
 use std::io::{self, BufReader, prelude::*};
 
+use algorithms::wires;
 
 fn main() -> io::Result<()> {
-    let intcode_file = File::open("data/intcode_prog.txt")?;
-    let intcode_reader = BufReader::new(intcode_file);
-    let mut code: Vec<_> = intcode_reader
-        .split(b',')
-        .map(|x| {
-                String::from_utf8(x.unwrap()).unwrap().parse::<i32>().unwrap()
-        })
-        .collect();
+    let wire_file = File::open("data/wires.txt")?;
+    let wire_reader = BufReader::new(wire_file);
+    let mut wire_line_iter = wire_reader.lines();
+    let wire_1 = wire_line_iter.next().unwrap()?.parse().unwrap();
+    let wire_2 = wire_line_iter.next().unwrap()?.parse().unwrap();
 
-    for noun in 0..code.len() {
-        for verb in 0..code.len() {
-            code[1] = noun as i32;
-            code[2] = verb as i32;
-            let mut machine = intcode::Machine::new(code.clone());
-            if let Ok(n) = machine.run() {
-                if n == 19690720 {
-                    println!("Noun={}, Verb={}", noun, verb);
-                }
-            }
-        }
-    }
+    println!("{:?}", wires::closest_wire_intersection(&wire_1, &wire_2));
+
     Ok(())
 }
