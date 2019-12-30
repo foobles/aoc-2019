3ab64df183509d04c2531b6fbf041aab7d94cecf
foobles <buzdav2@gmail.com>
2019-12-29 19:54:49 -0600

    added password verifier + counter

--- src/algorithms/mod.rs
+++ src/algorithms/mod.rs
@@ -9,3 +9,26 @@ pub fn module_fuel_req(mass: i32) -> i32 {
     })
     .sum()
 }
+
+fn is_valid_password(mut pw: i32) -> bool {
+    let mut dup_found = false;
+    let mut digits = 1;
+    let mut prev = pw % 10;
+    pw /= 10;
+    while pw > 0 {
+        digits += 1;
+        let cur = pw % 10;
+        if prev == cur {
+            dup_found = true;
+        } else if prev < cur {
+            return false;
+        }
+        prev = cur;
+        pw /= 10;
+    }
+    dup_found && digits == 6
+}
+
+pub fn password_count(lower: i32, upper: i32) -> usize {
+    (lower..=upper).filter(|&x| is_valid_password(x)).count()
+}
--- src/algorithms/tests.rs
+++ src/algorithms/tests.rs
@@ -9,3 +9,24 @@ fn algo_module_fuel_req() {
     assert_eq!(module_fuel_req(100756), 50346);
 }
 
+#[test]
+fn algo_test_password() {
+    assert!(is_valid_password(111111));
+    assert!(is_valid_password(122345));
+    assert!(is_valid_password(566699));
+}
+
+#[test]
+fn algo_test_password_wrong_digits() {
+    assert!(!is_valid_password(122));
+}
+
+#[test]
+fn algo_test_password_not_increasing() {
+    assert!(!is_valid_password(122340));
+}
+
+#[test]
+fn algo_test_password_no_double() {
+    assert!(!is_valid_password(123456));
+}
