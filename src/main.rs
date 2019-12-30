6dfeb8d759233ca48f308ee45894ace329c7db91
foobles <buzdav2@gmail.com>
2019-12-29 22:14:00 -0600

    solved password count

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,7 @@ use std::io::{self, BufReader, prelude::*};
 use algorithms::wires;
 
 fn main() -> io::Result<()> {
-    let wire_file = File::open("data/wires.txt")?;
-    let wire_reader = BufReader::new(wire_file);
-    let mut wire_line_iter = wire_reader.lines();
-    let wire_1 = wire_line_iter.next().unwrap()?.parse().unwrap();
-    let wire_2 = wire_line_iter.next().unwrap()?.parse().unwrap();
-
-    println!("{:?}", wires::closest_wire_intersection(&wire_1, &wire_2));
+    println!("Possible passwords = {}", algorithms::password_count(235741, 706948));
 
     Ok(())
 }
