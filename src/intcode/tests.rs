5fd88bf84d8585fa5bf98f62c55d83beb4dfac90
foobles <buzdav2@gmail.com>
2020-01-05 21:47:25 -0600

    added tests for jt, jf, eq, lt, in, and out

--- src/intcode/tests.rs
+++ src/intcode/tests.rs
@@ -9,6 +9,14 @@ fn test_machine_states(input: &[i32], output: &[i32]) {
     assert_eq!(machine.code.as_slice(), output);
 }
 
+fn test_machine_output(machine: &[i32], input: &[i32], output: &[i32]) {
+    let mut machine = Machine::new(machine.to_vec());
+    assert_eq!(
+        machine.run(input.iter().copied()).unwrap().as_slice(),
+        output
+    );
+}
+
 #[test]
 fn intcode_machine_add() {
     test_machine_states(
@@ -54,5 +62,71 @@ fn intcode_machine_lhs_immediate_add() {
     test_machine_states(
         &[101, 50, 4, 4, 49],
         &[101, 50, 4, 4, 99]
-    )
+    );
+}
+
+#[test]
+fn intcode_machine_cat() {
+    test_machine_output(
+        &[3, 0, 4, 0, 99],
+        &[123],
+        &[123]
+    );
+}
+
+#[test]
+fn intcode_machine_jt() {
+    test_machine_output(
+        &[1105, 0, 5, 104, 999, 99],
+        &[],
+        &[999]
+    );
+}
+
+#[test]
+fn intcode_machine_jf() {
+    test_machine_output(
+        &[6, 6, 7, 104, 999, 99, 0, 5],
+        &[],
+        &[]
+    );
+}
+
+#[test]
+fn intcode_machine_lt_less() {
+    test_machine_states(
+        &[1107, 99, 100, 0, 99],
+        &[1, 99, 100, 0, 99]
+    );
+}
+
+#[test]
+fn intcode_machine_lt_equal() {
+    test_machine_states(
+        &[7, 4, 4, 0, 99],
+        &[0, 4, 4, 0, 99],
+    );
+}
+
+#[test]
+fn intcode_machine_lt_greater() {
+    test_machine_states(
+        &[1007, 0, 6, 0, 99],
+        &[0, 0, 6, 0, 99],
+    );
+}
+
+#[test]
+fn intcode_machine_eq_equal() {
+    test_machine_states(
+        &[108, 2, 5, 1, 99, 2],
+        &[108, 1, 5, 1, 99, 2]
+    );
+}
+
+fn intcode_machine_eq_not_equal() {
+    test_machine_states(
+        &[1108, 5, 6, 2, 99],
+        &[1108, 5, 0, 2, 99]
+    );
 }
