3a22e326f14308c73d8bcc1caa3ce5ecb4997cb4
foobles <buzdav2@gmail.com>
2020-01-09 11:44:38 -0600

    score_setting() now accepts I: IntoIterator<Item=i32> instead of &[i32]

--- src/algorithms/amplifier/mod.rs
+++ src/algorithms/amplifier/mod.rs
@@ -1,7 +1,10 @@
 use crate::intcode::{Error, Machine};
 
-pub fn score_setting(machine: &Machine, setting: &[i32]) -> Result<Option<i32>, Error> {
-    setting.iter().fold(Ok(Some(0)), |p, &s| {
+pub fn score_setting<I>(machine: &Machine, setting: I) -> Result<Option<i32>, Error>
+where
+    I: IntoIterator<Item = i32>
+{
+    setting.into_iter().fold(Ok(Some(0)), |p, s| {
         p?.map_or(Ok(None), |input| {
             machine.clone()
                 .run([s, input].iter().copied())
